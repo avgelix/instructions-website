cb3e7dc30dbe2d317fdd9238aa813936f0a82c0a
Angela <apetrone@hawk.iit.edu>
2021-11-29 12:58:48 -0600

    Add a Javascript file. Add buttons in HTML. Write JavaScript functions to hide/show elements on click

---
+++ main.js
@@ -0,0 +1,79 @@
+
+// document.querySelectorAll("button").addEventListener
+
+// document.querySelectorAll("button").onclick = function() {
+  // document.getElementById("read-more-text-six").style.display='block';
+// } //
+
+
+function myFunctionOne() {
+  var x = document.getElementById("read-more-text-one");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+};
+
+function myFunctionTwo() {
+  var x = document.getElementById("read-more-text-two");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+};
+
+function myFunctionThree() {
+  var x = document.getElementById("read-more-text-three");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+};
+
+  function myFunctionFour() {
+    var x = document.getElementById("read-more-text-four");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  };
+
+function myFunctionFive() {
+  var x = document.getElementById("read-more-text-five");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+};
+
+function myFunctionSix() {
+  var x = document.getElementById("read-more-text-six");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+};
+
+function myFunctionSeven() {
+  var x = document.getElementById("read-more-text-seven");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+};
+
+function myFunctionEight() {
+  var x = document.getElementById("read-more-text-eight");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+};
